f483c1192ea15d3ec8f865e6988b6d7e7515f1e6
ivbz <iv@bitzlato.com>
2019-01-19 20:03:54 +0300

    Random orders optional. Allow change order amount

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const { startApp, stopApp } = require('./lib/mirror');
 program
     .option('--token <token>', 'API Token')
     .option('--api [api]', 'API endpoint', )
-    .option('--pause [pause]', 'Pause in ms between ticks')
+    .option('--pause [pause]', 'Pause in ms between ticks. Default 30000')
+    .option('--random [random]', 'Place random orders. Default false')
+    .option('--amount [amount]', 'Order amount in USD. Default 1 USD')
     .parse(process.argv);
 
 if (!program.token) {
@@ -16,7 +18,9 @@ if (!program.token) {
 const config = {
     token: program.token,
     apiUrl: program.api || 'https://bitzlato.com/api',
-    tick: program.pause || 30000
+    tick: program.pause || 30000,
+    random: program.random || false,
+    amount: program.amount || 1,
 };
 
 const appPromise = startApp(config);
