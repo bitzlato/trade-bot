93644f114321d67f2719fa54ba2639d8fd36ad1b
ivbz <iv@bitzlato.com>
2019-03-02 14:41:06 +0300

    different amount for bid and ask

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ program
     .option('--api [api]', 'API endpoint', )
     .option('--pause [pause]', 'Pause in ms between ticks. Default 30000')
     .option('--random [random]', 'Place random orders. Default false')
-    .option('--amount [amount]', 'Order amount in USD. Default 1 USD. Can be range, for example 1-10')
+    .option('--bid-amount [bidAmount]', 'Order amount in USD. Default 1 USD. Can be range, for example 1-10')
+    .option('--ask-amount [askAmount]', 'Order amount in USD. Default 1 USD. Can be range, for example 1-10')
     .option('--pairs [pairs]', 'List of comma-separated bitzlato pairs to work on. Default all')
     .option('--bid-levels [bidLevels]', 'List of comma-separated multipliers for buying orders. Default 1.0 0.95 0.9 0.85 0.8')
     .option('--ask-levels [askLevels]', 'List of comma-separated multipliers for selling orders. Default 1.0 1.05 1.1 1.15 1.2')
@@ -23,7 +24,8 @@ const config = {
     apiUrl: program.api || 'https://bitzlato.com/api',
     tick: program.pause || 30000,
     random: program.random || false,
-    amount: program.amount || '1',
+    bidAmount: program.bidAmount || '1',
+    askAmount: program.askAmount || '1',
     pairs: program.pairs || 'all',
     bidLevels: program.bidLevels || '1,0.95,0.9,0.85,0.8',
     askLevels: program.askLevels || '1,1.05,1.1,1.15,1.2',
